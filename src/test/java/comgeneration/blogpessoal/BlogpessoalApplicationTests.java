fde01c1fe26bc51ce13ade95ddd32984ee0f4b68
Dorivania Santos <dorim.dev@gmail.com>
2024-03-27 20:48:27 -0300

    Testes

--- src/test/java/comgeneration/blogpessoal/BlogpessoalApplicationTests.java
+++ src/test/java/comgeneration/blogpessoal/BlogpessoalApplicationTests.java
@@ -1,10 +1,17 @@
 package comgeneration.blogpessoal;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.TestInstance;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
+import org.springframework.test.context.ContextConfiguration;
 
-@SpringBootTest
-class BlogpessoalApplicationTests {
+import com.generation.blogpessoal.BlogpessoalApplication;
+
+@SpringBootTest(webEnvironment = WebEnvironment.RANDOM_PORT)
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+@ContextConfiguration(classes = BlogpessoalApplication.class)
+public abstract class BlogpessoalApplicationTests {
 
 	@Test
 	void contextLoads() {
